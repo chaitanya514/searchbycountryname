2f8cee63683a39d6e4bfed17ea7e3b98ac696542
Chaitanya Patel <patelchaitanya514@gmail.com>
2020-10-29 06:04:38 -0400

    first commmit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from "react";
 import './App.css';
+import axios from "axios";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [countries, setCountries] = useState([]);
+  
+ const [error, setError] = useState('');
+
+
+  useEffect(() => {
+    axios.get('https://restcountries.eu/rest/v2/all')
+
+        .then(res => {
+          console.log(res.data)
+            setCountries(res.data);
+         
+        })
+        .catch(err => {
+            setError(err.message);
+           
+        })
+        
+        
+},[]);
+function handleChange(e){
+  console.log(e.target.value)
+  setCountries(countries.filter((country)=>{
+    if(country.name.toLowerCase().includes(e.target.value)){
+      return country.name;
+    }
+  }))
+ 
+  }
+ const countryList =  countries.map((country,index)=>
+       <li key={index}>{country.name}</li>
+    )
+
+
+return(
+<div>
+ <input type="text" placeholder="search country name here" onChange={handleChange}/>
+  <ul>
+    {countryList}
+    </ul>
+ 
+  </div>)
 }
+
+
+  
 
 export default App;
